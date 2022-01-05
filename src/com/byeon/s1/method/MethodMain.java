ddf774dbe7801cb2d8ab945ea3dbebd432cfa2b0
BJS KING <97070463+BJSSSSSS@users.noreply.github.com>
2022-01-05 09:43:25 +0900

    Update Github

--- src/com/byeon/s1/method/MethodMain.java
+++ src/com/byeon/s1/method/MethodMain.java
@@ -9,6 +9,7 @@ public class MethodMain {
 	public static void main(String[] args) {
 		
 		System.out.println("MainMethod Start");
+		System.out.println("Git-hub Write");
 		
 		//객체 생성 코드
 		//new 클래스명();
